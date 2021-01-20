24608f9d870e300005c5b89c910df19341a6d727
aethiop <pinobirhanu3@gmail.com>
2021-01-20 02:47:00 -0600

    CPS experiment

--- geez.js
+++ geez.js
@@ -379,62 +379,107 @@ Environment.prototype = {
 	},
 };
 
-function evaluate(exp, env) {
+function evaluate(exp, env, callback) {
+	GUARD(evaluate, arguments);
 	switch (exp.type) {
 		case "num":
 		case "str":
 		case "bool":
-			return exp.value;
+			callback(exp.value);
+			return;
 
 		case "var":
-			return env.get(exp.value);
+			callback(env.get(exp.value));
+			return;
 
 		case "assign":
 			if (exp.left.type != "var")
 				throw new Error("ይህን መሰየም አልተቻለም፥ " + JSON.stringify(exp.left));
-			return env.set(exp.left.value, evaluate(exp.right, env));
+			evaluate(exp.right, env, function CC(right) {
+				GUARD(CC, arguments);
+				callback(env.set(exp.left.value, right));
+			});
+			return;
 
 		case "binary":
-			return apply_op(
-				exp.operator,
-				evaluate(exp.left, env),
-				evaluate(exp.right, env)
-			);
-
-		case "lambda":
-			return make_lambda(env, exp);
+			evaluate(exp.left, env, function CC(left) {
+				GUARD(CC, arguments);
+				evaluate(exp.right, env, function CC(right) {
+					GUARD(CC, arguments);
+					callback(apply_op(exp.operator, left, right));
+				});
+			});
+			return;
 
 		case "let":
-			exp.vars.forEach(function (v) {
-				var scope = env.extend();
-				scope.def(v.name, v.def ? evaluate(v.def, env) : false);
-				env = scope;
-			});
-			return evaluate(exp.body, env);
+			(function loop(env, i) {
+				GUARD(loop, arguments);
+				if (i < exp.vars.length) {
+					var v = exp.vars[i];
+					if (v.def)
+						evaluate(v.def, env, function CC(value) {
+							GUARD(CC, arguments);
+							var scope = env.extend();
+							scope.def(v.name, value);
+							loop(scope, i + 1);
+						});
+					else {
+						var scope = env.extend();
+						scope.def(v.name, false);
+						loop(scope, i + 1);
+					}
+				} else {
+					evaluate(exp.body, env, callback);
+				}
+			})(env, 0);
+
+		case "lambda":
+			callback(make_lambda(env, exp));
 
 		case "if":
-			var cond = evaluate(exp.cond, env);
-			if (cond !== false) return evaluate(exp.then, env);
-			return exp.else ? evaluate(exp.else, env) : false;
+			evaluate(exp.cond, env, function CC(cond) {
+				GUARD(CC, arguments);
+				if (cond !== false) evaluate(exp.then, env, callback);
+				else if (exp.else) evaluate(exp.else, env, callback);
+				else callback(false);
+			});
+			return;
 
 		case "prog":
-			var val = false;
-			exp.prog.forEach(function (exp) {
-				val = evaluate(exp, env);
-			});
-			return val;
+			(function loop(last, i) {
+				GUARD(loop, arguments);
+
+				if (i < exp.prog.length)
+					evaluate(exp.prog[i], env, function CC(val) {
+						GUARD(CC, arguments);
+						loop(val, i + 1);
+					});
+				else {
+					callback(last);
+				}
+			})(false, 0);
+			return;
 
 		case "call":
-			var func = evaluate(exp.func, env);
-			return func.apply(
-				null,
-				exp.args.map(function (arg) {
-					return evaluate(arg, env);
-				})
-			);
+			evaluate(exp.func, env, function CC(func) {
+				GUARD(CC, arguments);
+				(function loop(args, i) {
+					GUARD(loop, arguments);
+					if (i < exp.args.length)
+						evaluate(exp.args[i], env, function CC(arg) {
+							GUARD(CC, arguments);
+							args[i + 1] = arg;
+							loop(args, i + 1);
+						});
+					else {
+						func.apply(null, args);
+					}
+				})([callback], 0);
+			});
+			return;
 
 		default:
-			throw new Error("I don't know how to evaluate " + exp.type);
+			throw new Error("እንዴት እደሆነ አላቅም " + exp.type);
 	}
 }
 
@@ -477,23 +522,44 @@ function apply_op(op, a, b) {
 	}
 	throw new Error("የተሳሳተ ስሌት ምልክት፥ " + op);
 }
-
 function make_lambda(env, exp) {
 	if (exp.name) {
 		env = env.extend();
 		env.def(exp.name, lambda);
 	}
-	function lambda() {
+	function lambda(callback) {
+		GUARD(lambda, arguments);
 		var names = exp.vars;
 		var scope = env.extend();
 		for (var i = 0; i < names.length; ++i)
-			scope.def(names[i], i < arguments.length ? arguments[i] : false);
-		return evaluate(exp.body, scope);
+			scope.def(
+				names[i],
+				i + 1 < arguments.length ? arguments[i + 1] : false
+			);
+		evaluate(exp.body, scope, callback);
 	}
 	return lambda;
 }
 
-/* -----[ entry point for NodeJS ]----- */
+var STACKLEN;
+function GUARD(f, args) {
+	if (--STACKLEN < 0) throw new Continuation(f, args);
+}
+function Continuation(f, args) {
+	this.f = f;
+	this.args = args;
+}
+
+function Execute(f, args) {
+	while (true)
+		try {
+			STACKLEN = 200;
+			return f.apply(null, args);
+		} catch (ex) {
+			if (ex instanceof Continuation) (f = ex.f), (args = ex.args);
+			else throw ex;
+		}
+}
 
 var globalEnv = new Environment();
 
@@ -505,14 +571,20 @@ globalEnv.def("time", function (func) {
 	}
 });
 
+globalEnv.def("ፃፍ", function (callback, val) {
+	console.log(val);
+	callback(false);
+});
+
+globalEnv.def("halt", function (k) {});
+
+globalEnv.def("sleep", function (k, milliseconds) {
+	setTimeout(function () {
+		Execute(k, [false]); // continuations expect a value, pass false
+	}, milliseconds);
+});
 if (typeof process != "undefined")
 	(function () {
-		globalEnv.def("ፃፍ", function (val) {
-			console.log(val);
-		});
-		globalEnv.def("መስ_ፃፍ", function (val) {
-			console.log(val, "/n");
-		});
 		var code = "";
 		process.stdin.setEncoding("utf8");
 		process.stdin.on("readable", function () {
@@ -522,6 +594,20 @@ if (typeof process != "undefined")
 
 		process.stdin.on("end", function () {
 			var ast = parse(TokenStream(InputStream(code)));
-			evaluate(ast, globalEnv);
+			evaluate(ast, globalEnv, function (result) {
+				console.log("Here's the result");
+				console.log(result);
+			});
 		});
 	})();
+
+code = `ፃፍ("ሰላም")፤ፃፍ("ሀገር")`;
+ast = parse(TokenStream(InputStream(code)));
+
+Execute(evaluate, [
+	ast,
+	globalEnv,
+	function (result) {
+		console.log("*** Result:", result);
+	},
+]);
